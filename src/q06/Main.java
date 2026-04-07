0ee5aa62d0433895fc276fe40cc2f02ffe56f5fc
Vaishnavi Gupta <vaishnavigupta1228@gmail.com>
2026-04-07 10:49:10 +0530

    Calculate area of shapes based on user input

--- src/q06/Main.java
+++ src/q06/Main.java
@@ -1,15 +1,23 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        // TODO: Print first n Fibonacci numbers separated by spaces
-        //       F(0)=0, F(1)=1, F(n)=F(n-1)+F(n-2)
-        //
-        // Input: 8
-        // Output: 0 1 1 2 3 5 8 13
-
+        String shape = sc.next();
+        double area = 0;
+        if (shape.equalsIgnoreCase("circle")) {
+            double r = sc.nextDouble();
+            area = Math.PI * r * r;
+        } else if (shape.equalsIgnoreCase("rectangle")) {
+            double l = sc.nextDouble();
+            double w = sc.nextDouble();
+            area = l * w;
+        } else if (shape.equalsIgnoreCase("triangle")) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            double c = sc.nextDouble();
+            double s = (a + b + c) / 2.0;
+            area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+        System.out.printf("Area: %.2f", area);
     }
 }
